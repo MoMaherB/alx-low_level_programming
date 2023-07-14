565441ad28b0a50e27f2bb4117d1f0503d50fa58
MoMaherB <maher1525@gmail.com>
2023-07-14 11:15:43 -0700

    shell script task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Positive or negative
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+
+	{
+		putchar("%c\n", ch);
+		ch++;
+	}
+}
+
