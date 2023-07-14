b0d5aa955e8dd1198c75f3ece5192697b82459f7
MoMaherB <maher1525@gmail.com>
2023-07-13 17:27:52 -0700

    shell script task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program
+ *
+ * Return: i dont care
+ *
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1.quo.59);
+	return (1);
+}
