6c092ec51a15b4ce934d36cd8a04f82e2166ed17
MoMaherB <maher1525@gmail.com>
2023-07-13 17:39:00 -0700

    shell script task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Description: A C program
+ * Description: A C program that writes a quote to the standard output
  *
- * Return: i dont care
- *
-*/
-
+ * Return: Always 1 (error)
+ */
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1.quo.59);
+	write(1, quo, 59);
 	return (1);
 }
