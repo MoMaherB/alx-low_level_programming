6a75167c1417b27304829e725ca9d9657a65bec0
MoMaherB <maher1525@gmail.com>
2023-07-13 17:13:45 -0700

    shell script task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with wgrgrg
+ *
+ * Return: i guess som thing
+*/
+
+int main(void)
+{
+	printf("Size of a char: %1u byte(s)\n", sizeof(char));
+	printf("Size of an int: %1u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %1u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %1u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %1u byte(s)\n", sizeof(float));
+	return (0);
+}
