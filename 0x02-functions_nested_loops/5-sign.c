02ead7b3647b57f2fae00f10f64394712885db2a
MoMaherB <maher1525@gmail.com>
2023-07-18 10:43:33 -0700

    shell script task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		return (putchar('+'));
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		return (putchar('0'));
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		return (putchar('-'));
 		return (-1);
 	}
 }
