ec3febe8e22b268bde02d99ca209b8c5a4fb1bc4
MoMaherB <maher1525@gmail.com>
2023-07-18 13:51:27 -0700

    shell script task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,41 @@
-#include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _abs - Checks if a character is lowercase.
+ * main - Entry point
  *
- * @n: The character to be checked.
+ * Description: Positive or negative
  *
- * Return: abs(int).
+ * Return: 0 (success)
 */
-int _abs(int n)
+
+int main(void)
 {
-	return (abs(n));
+	int count, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+
+	for (count = 1; count <= 98; count++)
+	{
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			initial0s--;
+		}
+		printf("%lu", f1);
+
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) % mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
+
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
+	}
+	return (0);
 }
