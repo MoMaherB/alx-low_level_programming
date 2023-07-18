922860abee0f5a061537802ba31c4a8a5ddc6557
MoMaherB <maher1525@gmail.com>
2023-07-18 11:45:59 -0700

    shell script task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,9 +8,9 @@ void times_table(void)
 {
 	int i, j, sum;
 
-	for (i = 0; i >= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j >= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			sum = i * j;
 			_putchar(sum + '0');
