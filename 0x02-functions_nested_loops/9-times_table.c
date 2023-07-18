c093736cd83790252d95b9d30649beb4d9aca2eb
MoMaherB <maher1525@gmail.com>
2023-07-18 11:57:49 -0700

    shell script task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,8 +20,8 @@ void times_table(void)
 			if (sum <= 9)
 				_putchar(' ');
 			else
-				_putchar((sum / 10) + '0');
-				_putchar((sum % 10) + '0');
+				__putchar((sum / 10) + '0');
+			_putchar((sum % 10) + '0');
 		}
 		_putchar('\n');
 	}
