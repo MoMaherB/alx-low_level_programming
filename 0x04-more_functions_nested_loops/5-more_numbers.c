b759fe4d4457ac8756d0bbb3d8e88b5aea6a9b36
MoMaherB <maher1525@gmail.com>
2023-07-20 04:27:21 -0700

    shell script task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,20 @@
 
 void more_numbers(void)
 {
-	int count_num, count_lines;
+	int count_num, count_lines, num;
 
 	for (count_lines = 0; count_lines <= 9; count_lines++)
 	{
 		for (count_num = 0; count_num <= 14; count_num++)
-			printf("%i", count_num);
+		{
+			num = count_num;
+			if (count_num > 9)
+			{
+			_putchar(1 + '0');
+			num = count_num - 10;
+			}
+			_putchar(num + '0');
+		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
