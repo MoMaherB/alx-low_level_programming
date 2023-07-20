fc5384043be33101a28c418ebb12eb5da21a4ca3
MoMaherB <maher1525@gmail.com>
2023-07-20 07:39:11 -0700

    shell script task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio>
+
+/**
+ * main -Entry point
+ *
+ * Description: A Fizz Buzz Program
+ *
+ * Return: fizz Buzz
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if ((count % 3 == 0) && (count % 15 != 0))
+			printf("Fizz ");
+		else if ((count % 5 == 0) && (count % 15 != 0))
+			printf("Buzz ");
+		else if (count % 15 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%i ", count);
+	}
+	printf("\n");
+}
