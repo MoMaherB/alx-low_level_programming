bb727304835a8aba85bafe7d5ef744293f55420c
MoMaherB <maher1525@gmail.com>
2023-07-20 08:56:19 -0700

    shell script task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,7 @@ int main(void)
 	{
 		if ((count % 3 == 0) && !(count % 15 == 0))
 			printf("Fizz");
-		else if ((count % 5 == 0) && !(count % 13 == 0))
+		else if ((count % 5 == 0) && !(count % 15 == 0))
 			printf("Buzz");
 		else if (count % 15 == 0)
 			printf("FizzBuzz");
