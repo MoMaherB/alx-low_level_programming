20632ae50a40eebf859b561255361fa0c918575e
MoMaherB <maher1525@gmail.com>
2023-07-20 02:44:49 -0700

    shell script task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - This function multiply two numbers
+ *
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Returns: the multiplication result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
