43d1f95e5e17f4991bb4e121d00346974bf4da7b
MoMaherB <maher1525@gmail.com>
2023-07-24 06:01:17 -0700

    shell script task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - function that takes a pointer to an int parametr
+ *
+ *@a: ...do somthing
+ *@b: ...dff
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+	swap = *a;
+	a* = *b;
+	*b = swap;
+
+}
