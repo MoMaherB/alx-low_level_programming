bad73086771f28777661b881c8df81d595a703fb
MoMaherB <maher1525@gmail.com>
2023-07-24 11:57:23 -0700

    shell script task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
 /**
- * print_array - function atakes a pointer to an int parametr
+ * *_strcpy - function atakes a pointer to an int parametr
  *
- *@a: ...do somthing
- *@n: gogogog
+ *@dest: ...do somthing
+ *@src: gogogog
  * Return: length
  */
 
-void print_array(int *a, int n)
+char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = -1;
 
-	for (i = 0; i < n; i++)
-	{
-		printf("%d", a[i]);
-		if (i < n - 1)
-		{
-			printf(", ");
-		}
-	}
-	printf("\n");
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0';
+
+	return (dest);
 }
