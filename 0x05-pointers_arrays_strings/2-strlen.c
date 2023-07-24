4de3461c1ad6727c4250cfb6ee1e96f591bde0a0
MoMaherB <maher1525@gmail.com>
2023-07-24 08:30:36 -0700

    shell script task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,19 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strlen - function that takes a pointer to an int parametr
  *
  *@s: ...do somthing
+ *
+ * Return: length
  */
 
 int _strlen(char *s)
 {
-	int strl;
-	strl = strlen(const char *s);
-	return (strl);
+	int counter;
 
+	for (counter = 0; *s != '\0'; s++)
+		++counter;
+
+	return (counter);
 }
