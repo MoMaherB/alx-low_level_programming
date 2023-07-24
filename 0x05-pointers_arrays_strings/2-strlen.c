929c11d0bc3c7866089eb053370a4240e89a933e
MoMaherB <maher1525@gmail.com>
2023-07-24 06:16:33 -0700

    shell script task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function that takes a pointer to an int parametr
+ *
+ *@s: ...do somthing
+ */
+
+int _strlen(char *s);
+{
+	int strl;
+	strl = strlen(char *s);
+	return (strl);
+
+}
