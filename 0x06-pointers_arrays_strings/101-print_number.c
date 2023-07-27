febbad28c3773e002c2ba12d976d24068309a45a
MoMaherB <maher1525@gmail.com>
2023-07-27 16:59:35 -0700

    shell script task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer to the standard output.
+ * @n: The integer to be printed.
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	
+	int divisor = 1;
+	int temp = n;
+	
+	while (temp > 9)
+	{
+		temp /= 10;
+		divisor *= 10;
+	}
+
+	while (divisor >= 1)
+	{
+		_putchar((n / divisor) + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
+}
