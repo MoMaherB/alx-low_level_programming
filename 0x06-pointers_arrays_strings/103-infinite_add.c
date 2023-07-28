4b8b4c2fa67b0eef7cc11acb5904554f960e41f5
MoMaherB <maher1525@gmail.com>
2023-07-27 18:18:45 -0700

    shell script task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,52 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * infinite_add - Adds two numbers represented as strings.
+ * @n1: The first number as a string.
+ * @n2: The second number as a string.
+ * @r: The buffer that will store the result.
+ * @size_r: The size of the buffer.
+ * Return: A pointer to the result or 0 if the result cannot be stored in r.
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len_n1 = strlen(n1);
-    int len_n2 = strlen(n2);
-    int carry = 0;
-    int sum;
-    int i = len_n1 - 1;
-    int j = len_n2 - 1;
-    int idx = 0;
+	int len_n1 = strlen(n1), int len_n2 = strlen(n2), int carry = 0;
+	int sum, int i = len_n1 - 1, int j = len_n2 - 1, int idx = 0;
+	int start = 0, int end = idx - 1, char temp;
 
-    // Check if the result can be stored in r
-    if (len_n1 + len_n2 + 1 > size_r)
-        return 0;
+	if (len_n1 + len_n2 + 1 > size_r)
+		return (0);
 
-    // Add the numbers starting from the least significant digits
-    while (i >= 0 || j >= 0 || carry > 0)
-    {
-        sum = carry;
+	while (i >= 0 || j >= 0 || carry > 0)
+	{
+		sum = carry;
 
-        if (i >= 0)
-            sum += n1[i] - '0';
-        if (j >= 0)
-            sum += n2[j] - '0';
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
 
-        r[idx] = (sum % 10) + '0';
-        carry = sum / 10;
+		r[idx] = (sum % 10) + '0';
+		carry = sum / 10;
 
-        idx++;
-        i--;
-        j--;
-    }
+		idx++;
 
-    r[idx] = '\0';
+		i--;
+		j--;
+	}
+	r[idx] = '\0';
 
-    // Reverse the result to get the correct order
-    int start = 0;
-    int end = idx - 1;
-    char temp;
+	while (start < end)
 
-    while (start < end)
-    {
-        temp = r[start];
-        r[start] = r[end];
-        r[end] = temp;
-        start++;
-        end--;
-    }
+	{
+		temp = r[start];
+		r[start] = r[end];
+		r[end] = temp;
+		start++;
+		end--;
+	}
 
-    return r;
+	return (r);
 }
-
