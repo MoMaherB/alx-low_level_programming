b788450f1574de52cf92cbdbe3c4e050422c1d13
MoMaherB <maher1525@gmail.com>
2023-08-02 12:18:52 -0700

    shell script task

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _strlen - Helper function to calculate the length of a string.
+ * @s: The string whose length needs to be calculated.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen(s + 1));
+	}
+}
+
+/**
+ * is_palindrome_helper - Recursive helper
+ * function to check if a string is a palindrome.
+ * @s: The string to check.
+ * @start: The starting index of the string.
+ * @end: The ending index of the string.
+ *
+ * Return: 1 if the string is a palindrome, otherwise 0.
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+
+	return (is_palindrome_helper(s, start + 1, end - 1));
+}
+/**
+ * is_palindrome - Function to check if a string is a palindrome.
+ * @s: The string to check.
+ *
+ * Return: 1 if the string is a palindrome, otherwise 0.
+ */
+int is_palindrome(char *s)
+{
+	int length = _strlen(s);
+
+	return (is_palindrome_helper(s, 0, length - 1));
+}
