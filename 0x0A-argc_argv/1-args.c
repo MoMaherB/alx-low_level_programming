60bcdb3a6d851c2f7eac852190a00464ff056c59
MoMaherB <maher1525@gmail.com>
2023-08-04 11:25:05 -0700

    argm

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: somthing
+ * @argv: another
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%s\n", argc);
+	return (0);
+}
