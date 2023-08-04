2c094f5ec39526cc4c2be590c37817e9cc9797f4
MoMaherB <maher1525@gmail.com>
2023-08-04 16:23:57 -0700

    shell script task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: somthing
+ * @argv: another
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x, y;
+
+	if (argc > 3 || argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		y = atoi(argv[2]);
+		x = atoi(argv[1]);
+		printf("%d\n", x * y);
+		return (0);
+	}
+}
