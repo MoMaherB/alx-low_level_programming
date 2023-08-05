1b279d6f289edf528ded25de80e5c2e9666a6cf3
MoMaherB <maher1525@gmail.com>
2023-08-04 17:26:26 -0700

    shell script task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - Entry point
@@ -18,7 +17,7 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-			if (*c < '0' && *c > '9')
+			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
 		sum += atoi(argv[argc]);
 	}
