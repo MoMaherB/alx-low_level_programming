33f82d66cd88239173abc044446ed021559e9a69
MoMaherB <maher1525@gmail.com>
2023-08-08 12:47:59 -0700

    shell script task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * alloc_grid - creates arry of charachters
+ *
+ * @width: the width
+ * #height: the height
+ * Return: m or NUll
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	*grid = (int **)malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+
+		for (int j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
