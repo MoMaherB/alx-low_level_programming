76a358aa2abdffeeb968ed2b167a25d416a4ed1f
MoMaherB <maher1525@gmail.com>
2023-08-08 06:56:09 -0700

    shell script task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * create_array - creates arry of charachters
+ *
+ * @size: the size of array
+ * @c: the charachtr
+ * Return: n or NUll
+ * */
+
+char *create_array(unsigned int size, char c)
+{
+	char *n = malloc(size);
+
+	if (size == NULL || n == NULL)
+		return (NULL);
+	while (size--)
+		n[size] = c;
+	return (n);
+}
