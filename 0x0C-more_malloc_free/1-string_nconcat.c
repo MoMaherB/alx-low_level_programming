74581fe670fc2191cf99f166c8a4463ff752a0a1
MoMaherB <maher1525@gmail.com>
2023-08-10 04:47:46 -0700

    shell script task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <string.h>
+/**
+ * string_nconcat - The function
+ *
+ *@s1: first string
+ *@s2: second string
+ *@n: the number of nconcat
+ *Return: NULL or size
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int l1 = strlen(s1), int l2 = strlen(s2);
+	int i, j;
+	char *concat = (char *)malloc(l1 + n + 1);
+
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	if (s1 == NULL)
+
+	{
+		s1 = "";
+	}
+	if (s2 == Null)
+	{
+		s2 = "";
+	}
+	for (i = 0; i < l1; i++)
+	{
+		concat[i] = s1[i];
+	}
+	for (j = 0; j < (l1 + n); j++)
+	{
+		concat[l1 + j] = s2[j];
+	}
+	concat[l1 + n] = '\0';
+
+	return (concat);
+}
