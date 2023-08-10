aa0838eb3b9029d04546e0a02b419aed74a55795
MoMaherB <maher1525@gmail.com>
2023-08-10 05:41:54 -0700

    shell script task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,14 +16,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	char *concat;
 
-	concat = malloc(len1 + n + 1);
-
-	if (concat == NULL)
-		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	concat = malloc(len1 + n + 1);
+
+	if (concat == NULL)
+		return (NULL);
 	for (i = 0; i < len1; i++)
 	{
 		concat[i] = s1[i];
