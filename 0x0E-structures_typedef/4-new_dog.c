407a6ee7cd65303d77e79e238cc1030067ba7f13
MoMaherB <maher1525@gmail.com>
2023-08-14 15:50:04 -0700

    shell script task

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new instance of struct dog
+ * @name: Pointer to the name of the dog
+ * @age: Age of the dog
+ * @owner: Pointer to the owner's name
+ *
+ * Return: Pointer to the new struct dog, or NULL if memory allocation fails
+ *
+ * Description: This function creates a
+ * new struct dog instance and copies the provided
+ * name and owner strings to ensure they are stored independently.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->age = age;
+
+	new_dog->owner = strdup(owner);
+
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	return (new_dog);
+}
