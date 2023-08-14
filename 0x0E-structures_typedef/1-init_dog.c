7137616a3fbe19c5f76e1beb7d89f373c65c6fde
MoMaherB <maher1525@gmail.com>
2023-08-14 15:21:03 -0700

    shell script task

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,16 +1,16 @@
 #include "dog.h"
 
 /**
- * init_dog - Initializes a variable of type struct dog
+ * init_dog - Initializes a variable
  * @d: Pointer to the struct dog to be initialized
  * @name: Pointer to the name of the dog
  * @age: Age of the dog
  * @owner: Pointer to the owner's name
  *
  * Description: This function initializes
- * the members of a struct dog with the
- * provided name, age, and owner's name.
+ * Return: void.
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
