549f324df7f6976a23131bbbb5178cdd1b2bfba7
MoMaherB <maher1525@gmail.com>
2023-08-16 13:06:12 -0700

    shell script task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -15,23 +14,20 @@ int main(int argc, char **argv)
 	int (*operation)(int, int);
 
 	if (argc != 4)
-	{
 		printf("Error\n"), exit(98);
-	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+
 	operation = get_op_func(argv[2]);
         if (!operation)
-        {
-                printf("Error\n"), exit(99);
-        }
+		printf("Error\n"), exit(99);
+
 	if (!num2 && (argv[2][0] == '/' || argv[2][0] == '%'))
-	{
 		printf("Error\n"), exit(100);
-	}
 
 	result = operation(num1, num2);
 	printf("%d\n", result);
+
 	return (0);
 }
