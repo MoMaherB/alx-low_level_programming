45eee02ce0165bc78c9f3ec70184e47cf222a0a7
MoMaherB <maher1525@gmail.com>
2023-08-16 11:45:53 -0700

    shell script task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name using a provided function.
+ * @name: The name to be printed.
+ * @f: A function pointer that takes a char pointer as an argument.
+ *
+ * This function takes a name and a function pointer as arguments.
+ * It calls the provided function with the name as an argument,
+ * allowing the provided function to perform the printing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
