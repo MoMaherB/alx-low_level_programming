fcd0096c7b7993275ef30f5ab83f5887c46928c2
MoMaherB <maher1525@gmail.com>
2023-08-16 12:09:03 -0700

    shell script task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an integer in an array.
+ * @array: The array of integers.
+ * @size: The number of elements in the array.
+ * @cmp: A function pointer that takes an integer and returns an int.
+ *       This function is used to compare values.
+ *
+ * Return: The index of the intger
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+	return (-1);
+}
