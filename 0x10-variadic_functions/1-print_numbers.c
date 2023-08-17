7403a5001494e016d87cd28606991ddace0b3dc0
MoMaherB <maher1525@gmail.com>
2023-08-17 07:52:12 -0700

    shell script task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - the function that 
+ * sum_them_all - the function that
  * add any numbers the users enter
  *@n: the count
  *@separator: the separator by user
