212b8b32d942b0ace5b99c1c4134bf2e93d0f8d4
MoMaherB <maher1525@gmail.com>
2023-08-17 07:10:02 -0700

    shell script task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - the function that add any numbers the users enter
+ *@n: the count
+ *@separator: the separator by user
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, num;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
