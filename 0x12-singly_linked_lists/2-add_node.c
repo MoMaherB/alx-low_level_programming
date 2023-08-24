62f5bd3e186690ef947cfe5d704755759c2cb56a
MoMaherB <maher1525@gmail.com>
2023-08-24 16:06:25 -0700

    shell script task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,6 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 
 /**
  * add_node - at the beginning of a linked list add node
@@ -11,20 +11,20 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+	list_t *n;
+	unsigned int l = 0;
 
-	while (str[len])
-		len++;
+	while (str[l])
+		l++;
 
-	new = malloc(sizeof(list_t));
-	if (!new)
+	n = malloc(sizeof(list_t));
+	if (!n)
 		return (NULL);
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
+	n->str = strdup(str);
+	n->len = l;
+	n->next = (*head);
+	(*head) = n;
 
 	return (*head);
 }
