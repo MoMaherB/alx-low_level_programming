6169f092b22d5b16596edcc00373ed9730ac3be6
MoMaherB <maher1525@gmail.com>
2023-08-29 13:29:46 -0700

    shell script task

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add new node at beginning of the linked list
+ * @head: the pointer of first node in the list
+ * @n: data to insert in that new node
+ *
+ * Return: pointer to the new node, or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+	if (!newnode)
+		return (NULL);
+
+	newnode->n = n;
+	newnode->next = *head;
+	*head = newnode;
+
+	return (newode);
+}
