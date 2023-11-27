44ee35451bc93b8e28f84f2a52d4a7ec45b40896
MoMaherB <maher1525@gmail.com>
2023-11-27 10:51:59 -0800

    shell script task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * read_textfile- Rd txt to STDOUT.
+ * @filename: file to read name
+ * @letters: num of letters to be read
+ * Return: w- actual number of bytes or NULL
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fp, rd, wr;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fp = open(filename, 0_RDONLY);
+
+	if (fp == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	rd = read(fp, buffer, letters);
+	if (rd == -1)
+		return (0);
+
+	wr = write(STDOUT_FILENO, buffer, rd);
+	if (wr == -1)
+		return (0);
+
+	free(buffer);
+	close(fp);
+	return (wr);
+}
