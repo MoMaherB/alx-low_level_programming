e40ffcf221ba4de453c94ebd266677fcaa88c8c5
MoMaherB <maher1525@gmail.com>
2023-11-27 15:39:19 -0800

    shell script task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,9 +15,10 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	len = strlen(text_content);
 	if (text_content == NULL)
 		len = 0;
+	else
+		len = strlen(text_content);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
