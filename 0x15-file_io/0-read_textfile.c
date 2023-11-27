87018f620a85d21025e1721dd007014e40558487
MoMaherB <maher1525@gmail.com>
2023-11-27 11:03:59 -0800

    shell script task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fp = open(filename, 0_RDONLY);
+	fp = open(filename, O_RDONLY);
 
 	if (fp == -1)
 		return (0);
