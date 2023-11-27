6290b2eca8cd1b5ec095caf925a7ecd75fd0c824
MoMaherB <maher1525@gmail.com>
2023-11-27 15:23:16 -0800

    shell script task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * create_file - Creates file.
  * @filename: points toname of the file to create.
@@ -8,14 +8,13 @@
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
-
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, len;
 
 	len = strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC,0600);
 	if (fd == -1)
 		return (-1);
 
