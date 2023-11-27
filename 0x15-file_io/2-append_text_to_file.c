fbd21a766bf710726ba5ac5a6bdd40ab501dc891
MoMaherB <maher1525@gmail.com>
2023-11-27 15:45:09 -0800

    shell script task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	on = open(filename, O_WRONLY | O_APPEND);
 	wr = write(on, text_content, l);
-	
+
 	if (on == -1 || wr == -1)
 		return (-1);
 
