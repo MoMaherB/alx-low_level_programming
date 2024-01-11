723b32eb6e3306f35d4b7a085804e2875b5086b2
MoMaherB <maher1525@gmail.com>
2024-01-11 15:37:15 -0800

    Doubly linked lists

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -6,7 +6,6 @@
  * main - Generates and prints passwords for crackme5.
  * @argc: The number of arguments.
  * @argv: An array of pointers to the arguments.
- *
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
@@ -16,22 +15,17 @@ int main(int argc, char *argv[])
 	int len = strlen(argv[1]), i, temp;
 
 	(void)argc;
-
 	code = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-
 	temp = (len ^ 59) & 63;
 	pass[0] = code[temp];
-
 	temp = 0;
 	for (i = 0; i < len; i++)
 		temp += argv[1][i];
 	pass[1] = code[(temp ^ 79) & 63];
-
 	temp = 1;
 	for (i = 0; i < len; i++)
 		temp *= argv[1][i];
 	pass[2] = code[(temp ^ 85) & 63];
-
 	temp = 0;
 	for (i = 0; i < len; i++)
 	{
@@ -40,16 +34,13 @@ int main(int argc, char *argv[])
 	}
 	srand(temp ^ 14);
 	pass[3] = code[rand() & 63];
-
 	temp = 0;
 	for (i = 0; i < len; i++)
 		temp += (argv[1][i] * argv[1][i]);
 	pass[4] = code[(temp ^ 239) & 63];
-
 	for (i = 0; i < argv[1][0]; i++)
 		temp = rand();
 	pass[5] = code[(temp ^ 229) & 63];
-
 	pass[6] = '\0';
 	printf("%s", pass);
 	return (0);
