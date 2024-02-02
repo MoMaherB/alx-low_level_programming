6f63805d409d1b5a0e1c4ef2803576fdfdf3b36d
MoMaherB <maher1525@gmail.com>
2024-02-02 09:06:12 -0800

    C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,9 +4,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
-	if (size < 1)
-		return (NULL);
-
 	new_table = (hash_table_t *)malloc(sizeof(hash_table_t) * size);
 	if (new_table == NULL)
 		return (NULL);
