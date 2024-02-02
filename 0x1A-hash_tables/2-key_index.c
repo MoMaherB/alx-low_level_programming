5eb48b11f76e8fcd4baa676565034242d0c9f581
MoMaherB <maher1525@gmail.com>
2024-02-02 09:32:44 -0800

    C - Hash tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+/**
+ * key_index - Get the index of a key in a hash table
+ * @key: The key to find the index for
+ * @size: Size of the hash table
+ *
+ * Return:
+ * The index of the key in the hash table.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	return (hash_djb2(key) % size);
+}
